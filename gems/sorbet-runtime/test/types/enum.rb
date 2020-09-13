525139583ab0555c169df2439bcbfea74b0eb4aa
David Judd <djudd@stripe.com>
2020-09-12 22:02:59 -0700

    rubocop_todo: Style/StringConcatenation

--- gems/sorbet-runtime/test/types/enum.rb
+++ gems/sorbet-runtime/test/types/enum.rb
@@ -338,7 +338,7 @@ class T::Enum::Test::EnumTest < Critic::Unit::UnitTest
 
     it 'raises an assertion if to_str is called (implicitly) and also returns the serialized value' do
       ex = assert_raises(NoMethodError) do
-        'foo ' + CardSuit::HEART
+        'foo ' + CardSuit::HEART # rubocop:disable Style/StringConcatenation
       end
       assert_equal(ENUM_CONVERSION_MSG, ex.message)
     end
@@ -362,7 +362,7 @@ class T::Enum::Test::EnumTest < Critic::Unit::UnitTest
     end
 
     it 'raises an assertion if to_str is called (implicitly) and also returns the serialized value' do
-      assert_equal('foo heart', 'foo ' + CardSuit::HEART)
+      assert_equal('foo heart', 'foo ' + CardSuit::HEART) # rubocop:disable Style/StringConcatenation
     end
   end
 
