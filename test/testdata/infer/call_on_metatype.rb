90dca18cf9a42fc4160f77490981d4ba6664ff29
Nathan Froyd <froydnj@gmail.com>
2020-10-23 16:47:03 -0400

    remove some expressions that no longer pass the typechecker

--- test/testdata/infer/call_on_metatype.rb
+++ test/testdata/infer/call_on_metatype.rb
@@ -7,12 +7,6 @@ T.nilable(Integer).class # error: mistakes a type for a value
 
 T::Array[Integer].===(0) # error: mistakes a type for a value
 
-# This is a bug; there's a missing error here, hidden by https://github.com/sorbet/sorbet/issues/3426
-T.noreturn.to_s
-
-# This is also a bug; hidden by https://github.com/sorbet/sorbet/issues/3428
-T.untyped.to_s
-
 # And another one
 T.self_type.to_s
 
